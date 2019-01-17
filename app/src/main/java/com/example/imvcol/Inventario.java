05d14f7bef7d11d1486e6830dfd2c064c26be8dc
DMS <anjara3389@hotmail.com>
2019-01-17 18:06:38 -0500

    Initial commit

---
+++ app/src/main/java/com/example/imvcol/Inventario.java
@@ -0,0 +1,13 @@
+package com.example.imvcol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inventario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventario);
+    }
+}
