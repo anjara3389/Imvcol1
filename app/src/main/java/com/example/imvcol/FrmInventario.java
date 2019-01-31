9a042025cc2e41966174dcb539b0a89fb2fd999f
Neita <anjara3389@hotmail.com>
2019-01-31 08:49:54 -0500

    radioButtons mas grandes

--- app/src/main/java/com/example/imvcol/FrmInventario.java
+++ app/src/main/java/com/example/imvcol/FrmInventario.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.BadParcelableException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -203,6 +205,15 @@ public class FrmInventario extends AppCompatActivity implements YesNoDialogFragm
                 limpiar();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_action_bar, menu);
+        //menu.findItem(R.id.)
+        setTitle("INVCOL");
+        return true;
     }
 
     private void limpiar() {
